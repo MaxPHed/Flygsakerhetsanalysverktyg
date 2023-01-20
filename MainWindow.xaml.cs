0dcb5e4238c3306193173f25ef6d9e6db1d1a649
MaxPHed <114389278+MaxPHed@users.noreply.github.com>
2023-01-20 14:47:53 +0100

    buggrättningar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,25 +3,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml;
-using System.Xml.Linq;
-using K4os.Compression.LZ4.Internal;
-using MySql.Data.MySqlClient;
-using Org.BouncyCastle.Utilities;
 using Spire.Doc;
-using Spire.Doc.Documents;
 
 //using DataLibrary;
 
